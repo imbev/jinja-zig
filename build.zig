68a0c3fd4501ce4301af0ef5b4cbf0743ada2f1b
Isaac Beverly <imbev@protonmail.com>
2025-07-19 17:07:45 -0500

    Fix build.zig import

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
 
     const optimize = b.standardOptimizeOption(.{});
 
-    const jinja_zig = b.createModule(.{
+    const root_module = b.createModule(.{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
@@ -14,17 +14,17 @@ pub fn build(b: *std.Build) void {
     const lib = b.addLibrary(.{
         .linkage = .static,
         .name = "jinja_zig",
-        .root_module = jinja_zig,
+        .root_module = root_module,
     });
+
+    lib.root_module.addImport("jinja_zig", root_module);
 
     b.installArtifact(lib);
 
     const lib_unit_tests = b.addTest(.{
-        .root_module = jinja_zig,
+        .root_module = root_module,
     });
-
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
 }
